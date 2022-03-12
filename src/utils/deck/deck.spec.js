6e6827df7b3ce713d94d8bcd1f181e0ae556d0d6
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-12 16:59:38 +0100

    :white_check_mark: test(deck): add some starting unit tests

---
+++ src/utils/deck/deck.spec.js
@@ -0,0 +1,14 @@
+import { describe, it } from 'mocha';
+
+describe('Deck >>>', () => {
+  it.skip('should have 52 cards for 1 deck');
+  it.skip('should have 312 cards for 6 decks');
+
+  describe('Shuffle >', () => {
+    it.skip('should be idempotent given the same input');
+  });
+
+  describe('Draw >', () => {
+    it.skip('should remove cards from the top of the deck');
+  });
+});
