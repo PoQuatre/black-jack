1ea5c76eebac8895fc4adb8d26b89d7bfaabf4d6
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-15 10:13:11 +0100

    :construction: wip: add a `Card` component

--- src/views/game/Game.jsx
+++ src/views/game/Game.jsx
@@ -1,5 +1,11 @@
+import { Card } from '../../components';
 import styles from './Game.module.css';
 
 export default function Game() {
-  return <h1 className={styles.title}>Game</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>Game</h1>;
+      <Card />
+    </>
+  );
 }
