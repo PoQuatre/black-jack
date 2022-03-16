dbae5f1d397e59aa26d7fa6a58a61978b1944e82
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-16 10:52:16 +0100

    :sparkles: feat: save cards in `App.jsx` instead of `Game.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import { Start, Game, End } from './views';
 
 export default function App() {
+  const [dealerCards, setDealerCards] = useState([]);
+  const [playerCards, setPlayerCards] = useState([]);
+
+  const addDealerCard = (card) => {
+    setDealerCards((cards) => [...cards, card]);
+  };
+
+  const addPlayerCard = (card) => {
+    setPlayerCards((cards) => [...cards, card]);
+  };
+
+  const resetCards = () => {
+    setDealerCards([]);
+    setPlayerCards([]);
+  };
+
   return (
     <Routes>
       <Route path="/" element={<Start />} />
-      <Route path="/game" element={<Game />} />
+      <Route
+        path="/game"
+        element={
+          <Game
+            dealerCards={dealerCards}
+            playerCards={playerCards}
+            addDealerCard={addDealerCard}
+            addPlayerCard={addPlayerCard}
+            resetCards={resetCards}
+          />
+        }
+      />
       <Route path="/end" element={<End />} />
     </Routes>
   );
--- src/views/game/Game.jsx
+++ src/views/game/Game.jsx
@@ -4,30 +4,41 @@ import { Dealer, Hand } from '../../components';
 import { Deck, getScore } from '../../utils';
 import styles from './Game.module.css';
 
-export default function Game() {
+export default function Game({
+  dealerCards,
+  playerCards,
+  addDealerCard,
+  addPlayerCard,
+  resetCards,
+}) {
   const navigate = useNavigate();
   const deck = useRef(new Deck(6));
-
-  const [dealerCards, setDealerCards] = useState([]);
-  const [playerCards, setPlayerCards] = useState([]);
+  const [didMount, setDidMount] = useState(false);
 
   useEffect(() => {
-    setDealerCards((cards) => [...cards, deck.current.draw()]);
-    setPlayerCards((cards) => [...cards, deck.current.draw()]);
-    setDealerCards((cards) => [...cards, deck.current.draw()]);
-    setPlayerCards((cards) => [...cards, deck.current.draw()]);
-  }, []);
+    if (!didMount) {
+      resetCards();
+
+      addDealerCard(deck.current.draw());
+      addPlayerCard(deck.current.draw());
+      addDealerCard(deck.current.draw());
+      addPlayerCard(deck.current.draw());
+
+      setDidMount(true);
+    }
+  }, [addDealerCard, addPlayerCard, didMount, resetCards]);
 
   useEffect(() => {
+    if (!didMount) return;
     if (getScore(dealerCards) >= 21 || getScore(playerCards) >= 21) {
       navigate('/end');
     }
-  }, [navigate, dealerCards, playerCards]);
+  }, [navigate, dealerCards, playerCards, didMount]);
 
   const draw = () => {
-    setPlayerCards((cards) => [...cards, deck.current.draw()]);
+    addPlayerCard(deck.current.draw());
     if (getScore(dealerCards) < 16) {
-      setDealerCards((cards) => [...cards, deck.current.draw()]);
+      addDealerCard(deck.current.draw());
     }
   };
 
